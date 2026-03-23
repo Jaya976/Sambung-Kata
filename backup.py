9dedbfc345f73d966d0beb440070d01c80ae8e13
Dedealdian <email-githubmu@example.com>
2026-03-23 12:20:27 +0700

    Kirim DB sekali jalan tanpa polling

--- backup.py
+++ backup.py
@@ -2,26 +2,24 @@ import telebot
 import os
 from dotenv import load_dotenv
 
-# load_dotenv() gunanya untuk baca file .env jika di lokal/Termux
-# Di Railway, dia akan otomatis ambil dari tab Variables
 load_dotenv()
 
 TOKEN = os.getenv("TOKEN")
-OWNER_ID = os.getenv("OWNER_ID") # Pastikan di Railway namanya OWNER_ID
+OWNER_ID = os.getenv("OWNER_ID")
 
 bot = telebot.TeleBot(TOKEN)
 
-def kirim_ke_telegram():
+def kirim_langsung():
     try:
-        # Cek apakah file database ada
         if os.path.exists('bungkata.db'):
             with open('bungkata.db', 'rb') as doc:
-                bot.send_document(OWNER_ID, doc, caption="Ini Backup Database dari Railway!")
-            print("Database berhasil terkirim!")
+                bot.send_document(OWNER_ID, doc, caption="INI DATABASE KAMU!")
+            print("BERHASIL TERKIRIM!")
         else:
-            print("File bungkata.db tidak ditemukan di server.")
+            print("FILE DB TIDAK ADA")
     except Exception as e:
-        print(f"Terjadi kesalahan: {e}")
+        print(f"ERROR: {e}")
 
 if __name__ == "__main__":
-    kirim_ke_telegram()
+    kirim_langsung()
+    # JANGAN tambahkan bot.polling() di sini!
